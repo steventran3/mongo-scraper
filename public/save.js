03e2ae5478931b2874617749ab5c20616c1b677b
STEVEN TRAN <StevenTran@STEVENs-MacBook-Pro-2.local>
2018-02-21 13:21:51 -0800

    added more js

--- public/save.js
+++ public/save.js
@@ -5,12 +5,13 @@ $.getJSON("/saved/articles", data => {
         $("#saved_articles").append(`<div class ='card'> <div class='card-header'>${el.title}</div>
         <div class="card-body">
         <p class ='card-text '>${el.link}</p>
-        <button data-id=${el._id} type='button' class='btn btn-danger id='myBtn'>Add Note</button></div></div>`)
+        <button type='button' data-id=${el._id} id='save-article' class='btn btn-danger' data-toggle='modal' data-target='#note-article'>Add Notes</button></div></div>`)
     })
   });
-$(document).on('click','#myBtn', function(){
-    $('myModal').modal()
-    alert('hello')
+$(document).on('click','#save-article', function(){
+    
+    let id = $(this).attr('data-id')
+    console.log(id)
+    $('.modal-title').text(`Notes For Article: ${id}`)
+    $('modal').modal()
 })
-  
-
